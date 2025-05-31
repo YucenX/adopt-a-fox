059fb8310fbb47fac6e9394d4c18c85c767b6184
Yucen Xie <yucenxie5@gmail.com>
2025-05-31 18:18:41 -0400

    adoption form refactor - implementation efficiency + adopt more foxes!

--- fennec_form_component/__init__.py
+++
@@ -1,6 +0,0 @@
-import streamlit.components.v1 as components
-
-fennec_form_component = components.declare_component(
-    "fennec_form",
-    path="./fennec_form_component"
-)
--- st_pages/adoption.py
+++ st_pages/adoption.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from fennec_form_component import fennec_form_component
+from adoption_form_component import adoption_form_component
 
 st.set_page_config(layout="wide")
 
-st.markdown("# Adopt a Fennec Fox Pokémon!")
+st.markdown("# Adopt a Fox Pokémon!")
 
-submission = fennec_form_component(preselected="Braixen")
+submission = adoption_form_component(preselected="Braixen")
 
 if submission:
     st.success("Submitted successfully!")

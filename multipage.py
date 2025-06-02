99caaef2c687e405ff7caed213050c6d3921f307
Yucen Xie <yucenxie5@gmail.com>
2025-06-01 22:42:18 -0400

    make svelte page first for convenience

--- multipage.py
+++ multipage.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 pages = {
-    "Adoption": st.Page("st_pages/adoption.py", title="Adopt a Fox", icon="🦊"),
     "Breeding Tree": st.Page("st_pages/breeding_tree.py", title="Fox Breeding Tree", icon="💞"),
+    "Adoption": st.Page("st_pages/adoption.py", title="Adopt a Fox", icon="🦊"),
 }
 
 pg = st.navigation(list(pages.values()))
